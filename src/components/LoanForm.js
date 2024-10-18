52b7c4aac8d4b7e138ddd4072d80b16961cfa88b
matheus <matheusnun.ns@gmail.com>
2024-10-18 16:22:23 -0300

    [FEAT] added conditional state to show loan tax correction input

--- src/components/LoanForm.js
+++ src/components/LoanForm.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
-import { 
-  Button, 
-  FormControl, 
-  FormLabel, 
-  MenuItem, 
-  Select, 
-  TextField 
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  MenuItem,
+  Select,
+  TextField,
 } from "@mui/material";
-import '../styles/LoanForm.css';
-import { useFontStyles, useButtonStyles } from '../hooks/MUIComponentsStyles.js';
+import "../styles/LoanForm.css";
+import {
+  useFontStyles,
+  useButtonStyles,
+} from "../hooks/MUIComponentsStyles.js";
 
 const LoanForm = ({ onCalculate }) => {
   const [loanType, setLoanType] = useState("");
@@ -30,7 +33,7 @@ const LoanForm = ({ onCalculate }) => {
   };
 
   const handleSelectChange = (e) => {
-    const { value } = e.target; 
+    const { value } = e.target;
     setLoanType(value);
     setFormData({ ...formData, calculationOption: value });
   };
@@ -47,73 +50,90 @@ const LoanForm = ({ onCalculate }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}> 
-    <FormControl id='form'>
-      <FormLabel className={fontClasses.root}>Valor do Empréstimo</FormLabel>
-      <TextField
-        variant="outlined"
-        className={fontClasses.root}
-        name="loanAmount"
-        type="number"
-        onChange={handleChange}
-        required
-      />
-      <FormLabel className={fontClasses.root}>Taxa de Juros</FormLabel>
-      <TextField
-        variant="outlined"
-        className={fontClasses.root}
-        name="interestRate"
-        type="number"
-        onChange={handleChange}
-        required
-      />
-      <FormLabel className={fontClasses.root}>Número de Parcelas</FormLabel>
-      <TextField
-        variant="outlined"
-        className={fontClasses.root}
-        name="installments"
-        type="number"
-        onChange={handleChange}
-        required
-      />
-      <FormLabel className={fontClasses.root}>Mês e Ano de Concessão</FormLabel>
-      <TextField
-        variant="outlined"
-        className={fontClasses.root}
-        name="concessionDate"
-        type="month"
-        onChange={handleChange}
-        required
-      />
-      <FormLabel className={fontClasses.root}>Opção de Calculo</FormLabel>
-      <Select
-        value={loanType}
-        onChange={handleSelectChange}
-        required
-      >
-        <MenuItem value="preFixedInterestOB">Pré-fixado com juros sobre saldo devedor</MenuItem>
-        <MenuItem value="preFixedInterestInstallments">Pré-fixado com juros na parcela</MenuItem>
-        <MenuItem value="postFixedInterestCorrectionOB">Pós-fixado com juros e correção no saldo devedor</MenuItem>
-        <MenuItem value="postFixedInterestCorrectionInstallments">Pós-fixado com juros e correção na parcela</MenuItem>
-        <MenuItem value="postFixedInterestOBCorrectionInstallments">Pós-fixado com juros sobre saldo devedor e correção na parcela</MenuItem>
-      </Select>
-      <FormLabel className={fontClasses.root}>Taxa de Correção Monetária</FormLabel>
-      <TextField
-        variant="outlined"
-        className={fontClasses.root}
-        name="correctionRate"
-        type="number"
-        onChange={handleChange}
-        disabled={["preFixedInterestOB", "preFixedInterestInstallments"].includes(loanType)}
-        required
-      />
-      <Button 
-        className={buttonClasses.root}
-        type="submit"
-        variant="contained" 
-        id='button'
-      >Simular</Button>
-    </FormControl>
+    <form onSubmit={handleSubmit}>
+      <FormControl id="form">
+        <FormLabel className={fontClasses.root}>Valor do Empréstimo</FormLabel>
+        <TextField
+          variant="outlined"
+          className={fontClasses.root}
+          name="loanAmount"
+          type="number"
+          onChange={handleChange}
+          required
+        />
+        <FormLabel className={fontClasses.root}>Taxa de Juros</FormLabel>
+        <TextField
+          variant="outlined"
+          className={fontClasses.root}
+          name="interestRate"
+          type="number"
+          onChange={handleChange}
+          required
+        />
+        <FormLabel className={fontClasses.root}>Número de Parcelas</FormLabel>
+        <TextField
+          variant="outlined"
+          className={fontClasses.root}
+          name="installments"
+          type="number"
+          onChange={handleChange}
+          required
+        />
+        <FormLabel className={fontClasses.root}>
+          Mês e Ano de Concessão
+        </FormLabel>
+        <TextField
+          variant="outlined"
+          className={fontClasses.root}
+          name="concessionDate"
+          type="month"
+          onChange={handleChange}
+          required
+        />
+        <FormLabel className={fontClasses.root}>Opção de Calculo</FormLabel>
+        <Select value={loanType} onChange={handleSelectChange} required>
+          <MenuItem value="preFixedInterestOB">
+            Pré-fixado com juros sobre saldo devedor
+          </MenuItem>
+          <MenuItem value="preFixedInterestInstallments">
+            Pré-fixado com juros na parcela
+          </MenuItem>
+          <MenuItem value="postFixedInterestCorrectionOB">
+            Pós-fixado com juros e correção no saldo devedor
+          </MenuItem>
+          <MenuItem value="postFixedInterestCorrectionInstallments">
+            Pós-fixado com juros e correção na parcela
+          </MenuItem>
+          <MenuItem value="postFixedInterestOBCorrectionInstallments">
+            Pós-fixado com juros sobre saldo devedor e correção na parcela
+          </MenuItem>
+        </Select>
+        {["preFixedInterestOB", "preFixedInterestInstallments"].includes(
+          loanType
+        ) && (
+          <>
+            <FormLabel className={fontClasses.root}>
+              Taxa de Correção Monetária
+            </FormLabel>
+            <TextField
+              variant="outlined"
+              className={fontClasses.root}
+              name="correctionRate"
+              type="number"
+              onChange={handleChange}
+              required
+            />
+          </>
+        )}
+        <Button
+          className={buttonClasses.root}
+          type="submit"
+          variant="contained"
+          id="button"
+        >
+          Simular
+        </Button>
+      </FormControl>
     </form>
   );
 };
