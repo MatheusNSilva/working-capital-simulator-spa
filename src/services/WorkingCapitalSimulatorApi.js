a055c921172878b0eff2a1137e6fe0339c606f2f
matheus <matheusnun.ns@gmail.com>
2024-10-20 20:10:49 -0300

    [fix] route

--- src/services/WorkingCapitalSimulatorApi.js
+++ src/services/WorkingCapitalSimulatorApi.js
@@ -1,12 +1,10 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: process.env.NODE_API_URL || 'http://localhost:3000', // Substitua pela URL do seu backend se estiver hospedado
-});
+const apiURL = process.env.NODE_API_URL || 'http://localhost:3000'; 
 
 export const calculateInstallments = async (data) => {
   try {
-    const response = await api.post('/api/calculate-installments', data);
+    const response = await axios.post(`${apiURL}/api/calculate-installments`, data);
     return response.data;
   } catch (error) {
     console.error('Erro, não foi possível calcular as parcelas:', error);
